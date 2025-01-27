4704e032d071841d269299d51b74adecbd1369d5
Mary Connelly <connellymarykate@gmail.com>
2025-01-27 15:03:43 -0700

    save progress

--- snowPilotObs.py
+++ snowPilotObs.py
@@ -1,4 +1,6 @@
 from xml.dom import minidom
+from metaData import MetaData
+from user import User
 
 class SnowPilot_object(object):
 
@@ -11,16 +13,30 @@ class SnowPilot_object(object):
         self.doc = self.parse_file(self.file_path)
         self.root = self.doc.documentElement
 
-        self.metaData = self.set_metaData(self.doc)
-        self.locationInfo = self.set_locationInfo(self.doc)
-        self.pitInfo = self.set_pitInfo(self.doc)
+
+        self.date = self.set_date(self.doc, self.root)
+
+        #self.user = self.set_user(self.doc, self.root)
+        #self.locationInfo = self.set_locationInfo(self.doc)
+        #self.pitInfo = self.set_pitInfo(self.doc)
 
     def parse_file(self, file_path):
         return minidom.parse(file_path)
+    
+    #Setters
 
-    def set_metaData(self, doc):
-        metaData_list = doc.getElementsByTagName('caaml:metaData')
-        for child in metaData_list:
-            for subchild in child.childNodes:
-                print(subchild.nodeName)
-                print(subchild.firstChild.nodeValue)
+    def set_date(self, doc, root):
+
+        timeRef_list = root.getElementsByTagName('caaml:dateTimeReport')
+        dateTime=timeRef_list[0].firstChild.nodeValue
+        dateTime=dateTime.split('T')
+
+        return dateTime[0]
+
+
+    #Getters
+
+    def get_date(self):
+        return self.date
+
+
