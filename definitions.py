a0e8834e4965b6f9571d5a5e8668171cb6ed6b17
Mary Connelly <connellymarykate@gmail.com>
2025-03-03 16:44:37 -0700

    definitions

--- definitions.py
+++ definitions.py
@@ -1,4 +1,12 @@
 # Grain form dictionary
+def grain_form_name(grain_form):
+    grain_form_dict = {
+        "0": "undefined",
+        "1": "rounded",
+        "2": "angular",
+        "3": "faceted",
+    }
+    return grain_form_dict[grain_form]
 
 # Weather conditions dictionary
 
