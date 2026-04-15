fcaab4781d2cd1625fdabceb67947c097f95679d
Mary Kate (Connelly) Carara <connellymarykate@gmail.com>
2026-04-15 13:18:43 -0600

    bump version

--- src/snowpylot/__init__.py
+++ src/snowpylot/__init__.py
@@ -2,7 +2,7 @@
 SnowPylot - A Python library for working with CAAML snow profile data
 """
 
-__version__ = "1.1.3"
+__version__ = "1.1.5"
 
 from .caaml_parser import caaml_parser, caaml_url_parser
 from .snow_pit import SnowPit
